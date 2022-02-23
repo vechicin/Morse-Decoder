73a00728ab59682ff0aaa9c5562c3f78d98ca4e0
Santiago Velosa <santiagovelosa@gmail.com>
2022-02-23 15:25:16 -0500

    Create decode_word method

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,31 +1,31 @@
 MORSE_CODE_ALPHABET = {
-  ".-" => "A",
-  "-..." => "B",
-  "-.-." => "C",
-  "-.." => "D",
-  "." => "E",
-  "..-." => "F",
-  "--." => "G",
-  "...." => "H",
-  ".." => "I",
-  ".---" => "J",
-  "-.-" => "K",
-  ".-.." => "L",
-  "--" => "M",
-  "-." => "N",
-  "---" => "O",
-  ".--." => "P",
-  "--.-" => "Q",
-  ".-." => "R",
-  "..." => "S",
-  "-" => "T",
-  "..-" => "U",
-  "...-" => "V",
-  ".--" => "W",
-  "-..-" => "X",
-  "-.--" => "Y",
-  "--.." => "Z"
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def decode_char(char)
   MORSE_CODE_ALPHABET.each do |key, value|
@@ -33,4 +33,15 @@ def decode_char(char)
   end
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
+
+def decode_word(string)
+  alphabet_array = []
+  split_string = string.split
+  split_string.each do |letter|
+    alphabet_array.push(decode_char(letter))
+  end
+  alphabet_array.join
+end
+
+puts decode_word('-- -.--')
